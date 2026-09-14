a5f4454c4baab68d7b2e891ad5de656bbe21c9c6
yami <qjasonc@gmail.com>
2026-09-13 21:22:11 -0700

    Finished word guessing game

--- word-guessing-game/main.py
+++ word-guessing-game/main.py
@@ -6,21 +6,32 @@ word = random.choice(words)
 
 hidden  = []
 guesses = []
+lives = 6
 
-print(word)
 for i in range(len(word)):
     hidden.append("-")
 print(*hidden)
 
-while "-" in hidden:
-    guess = input(f"Guess a letter: ")
-    while guess in guesses:
-        guess = input(f"You already guessed that letter!\n Guess a letter: ")
+while "-" in hidden and lives != 0:
+    while True:
+        guess = input(f"\nGuess a letter: ")
+        while guess in guesses:
+            guess = input(f"You already guessed that letter!\nGuess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        print("Please only enter one letter: ")
     for i in range(len(word)):
         if guess == word[i]:
             hidden[i] = word[i]
+    if guess not in word:
+        print(f"Wrong guess")
+        lives -= 1
+        print(f"You have {lives} lives!")
     guesses.append(guess)
     print(*hidden)
 
-print("\nGood job!")
+if lives == 0:
+    print(f"Nice try!\nThe word was {word}")
+else:
+    print("Good job!")
 
