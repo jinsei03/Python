2739ee505c073031eeedfca05e4cb203a9b6ceec
yami <qjasonc@gmail.com>
2026-09-18 21:44:16 -0700

    fixed a bit of student gradebook

--- student-gradebook/main.py
+++ student-gradebook/main.py
@@ -1,6 +1,6 @@
 students = []
-choice = input("1. Add Student and Grades\n2. View all students\n3. Determine letter grade of student\n4. Quit\n: ")
-while choice != "4":
+while True:
+    choice = input("1. Add Student and Grades\n2. View all students\n3. Determine letter grade of student\n4. Quit\n: ")
     if choice == "1":
         name = input("What is the Student's name: ")
         grade1 = input("What is the students 1st grade: ")
@@ -13,23 +13,24 @@ while choice != "4":
             "Grade3": grade3
         }
         students.append(student_data)
+        print(students)
     elif choice == "2":
-        for sutdent_data in students:
-            print("Name: ", student_data["Name"])
-            print("Grades:",student_data["Grade1"],",",student_data["Grade2"],",",student_data["Grade3"])
+        for student in students:
+            print("Name: ", student["Name"])
+            print("Grades:",student["Grade1"],",",student["Grade2"],",",student["Grade3"])
     elif choice == "3":
-        for student_data in students:
-            grade = (int(student_data["Grade1"]) + int(student_data["Grade2"]) + int(student_data["Grade3"])) / 3
+        for student in students:
+            grade = (int(student["Grade1"]) + int(student["Grade2"]) + int(student["Grade3"])) / 3
             if grade >= 90:
-                print(student_data["Name"],"has a A")
+                print(student["Name"],"has a A")
             elif grade <= 89 and grade >= 80:
-                print(student_data["Name"], "has a B")
+                print(student["Name"], "has a B")
             elif grade <= 79 and grade >= 70:
-                print(student_data["Name"], "has a C")
+                print(student["Name"], "has a C")
             elif grade <= 69 and grade >= 60:
-                print(student_data["Name"], "has a D")
+                print(student["Name"], "has a D")
             else:
-                print(student_data["Name"], "has a F")
-                
-    choice = input("\n1. Add Student and Grades\n2. View all students\n3. Determine letter grade of student\n4. Quit\n: ")
+                print(student["Name"], "has a F")
+    elif choice == "4":
+        break
 print("Good bye!")
